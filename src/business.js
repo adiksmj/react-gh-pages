33de03e7d9ef1d0ea2a2f83962f8acb4fd677f6c
adiksmj <gedeadi6@gmail.com>
2021-08-31 15:21:10 +0800

    tes

--- src/business.js
+++ src/business.js
@@ -8,7 +8,7 @@ const Business = () => {
             </Helmet>
             <div className="content">
                 <h1>Our Business</h1>
-                <p>Consequat Lorem elit occaecat proident et aute do quis duis ullamco pariatur. Aliqua magna exercitation enim ad sint exercitation ullamco. Duis id quis ullamco nulla culpa culpa cillum do. Sit veniam tempor veniam ea in culpa. Dolor Lorem esse dolor labore sit quis esse consectetur velit Lorem proident.</p>
+                <p>Testing Consequat Lorem elit occaecat proident et aute do quis duis ullamco pariatur. Aliqua magna exercitation enim ad sint exercitation ullamco. Duis id quis ullamco nulla culpa culpa cillum do. Sit veniam tempor veniam ea in culpa. Dolor Lorem esse dolor labore sit quis esse consectetur velit Lorem proident.</p>
             </div>
             <style jsx>{`
                 .content {
